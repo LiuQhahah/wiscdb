00c3fb15620d448989a6aa1fd67d3c4fbb062568
qiang_liu <liuqiang9596@gmail.com>
2025-02-19 16:07:38 +0800

    add y function

--- y/utils.go
+++ y/utils.go
@@ -1,7 +1,1 @@
 package y
-
-import "hash/crc32"
-
-var (
-	CastTagNoLiCrcTable = crc32.MakeTable(crc32.Castagnoli)
-)
---
+++ y/y.go
@@ -0,0 +1,67 @@
+package y
+
+import (
+	"errors"
+	"hash/crc32"
+	"os"
+	"time"
+)
+
+var (
+	ErrEOF               = errors.New("ErrEOF: End of file")
+	ErrCommitAfterFinish = errors.New("batch commit not permitted after finish")
+)
+
+type Flags int
+
+const (
+	Sync Flags = 1 << iota
+	ReadOnly
+)
+
+var (
+	datasyncFileFlag    = 0x0
+	CastTagNoLiCrcTable = crc32.MakeTable(crc32.Castagnoli)
+)
+
+func OpenExistingFile(filename string, flags Flags) (*os.File, error) {
+	return nil, nil
+}
+func CreateSyncedFile(filename string, sync bool) (*os.File, error) {
+	return nil, nil
+}
+
+func OpenTruncateFile(filename string, sync bool) (*os.File, error) {
+	return nil, nil
+}
+func SafeCopy(a, src []byte) []byte {
+	return nil
+}
+
+func Copy(a []byte) []byte {
+	return nil
+}
+
+func KeyWithTs(key []byte, ts uint64) []byte {
+	return nil
+}
+
+func ParseTs(key []byte) uint64 {
+	return 0
+}
+
+func CompareKeys(key1, key2 []byte) int {
+	return 0
+}
+
+func ParseKey(key []byte) []byte {
+	return nil
+}
+
+func SameKey(str, dst []byte) bool {
+	return false
+}
+
+func FixedDuration(d time.Duration) string {
+	return ""
+}
