9ff54ef69ea0c2f146762eaf220511b633fbe174
qiang_liu <liuqiang9596@gmail.com>
2025-02-21 21:25:26 +0800

    add value method

--- y/valuestruct.go
+++ y/valuestruct.go
@@ -1,6 +1,9 @@
 package y
 
-import "bytes"
+import (
+	"bytes"
+	"encoding/binary"
+)
 
 type ValueStruct struct {
 	Meta      byte
@@ -10,17 +13,34 @@ type ValueStruct struct {
 	Version   uint64
 }
 
+// 无人调用
 func (v *ValueStruct) EncodeTo(buf *bytes.Buffer) {
-
+	buf.WriteByte(v.Meta)
+	buf.WriteByte(v.UserMeta)
+	var enc [binary.MaxVarintLen64]byte
+	sz := binary.PutUvarint(enc[:], v.ExpiresAt)
+	buf.Write(enc[:sz])
+	buf.Write(v.Value)
 }
 
+// 不通过buffer，没有写到buffer中
 func (v *ValueStruct) Encode(b []byte) uint32 {
-	return 0
+	b[0] = v.Meta
+	b[1] = v.UserMeta
+	sz := binary.PutUvarint(b[2:], v.ExpiresAt)
+	n := copy(b[2+sz:], v.Value)
+	return uint32(2 + sz + n)
 }
 func (v *ValueStruct) Decode(b []byte) {
+	v.Meta = b[0]
+	v.UserMeta = b[1]
+	var sz int
+	v.ExpiresAt, sz = binary.Uvarint(b[2:])
+	v.Value = b[2+sz:]
 
 }
 
 func (v *ValueStruct) EncodedSize() uint32 {
-	return 0
+	sz := binary.PutUvarint(make([]byte, 10), v.ExpiresAt)
+	return uint32(2 + sz + len(v.Value))
 }
