f6f0bffff76fda341c266886c41833cbcc24c01e
qiang_liu <liuqiang9596@gmail.com>
2025-03-28 17:44:04 +0800

    add binary int to []byte and []byte to int

--- y/error.go
+++ y/error.go
@@ -1,6 +1,7 @@
 package y
 
 import (
+	"encoding/binary"
 	"github.com/pkg/errors"
 	"log"
 )
@@ -15,4 +16,22 @@ func AssertTruef(b bool, format string, args ...interface{}) {
 	if !b {
 		log.Fatalf("%+v", errors.Errorf(format, args...))
 	}
+}
+
+// 将byte类型转化成uint32位，使用的binary的package采用大端序转化
+func BytesToU32(b []byte) uint32 {
+	return binary.BigEndian.Uint32(b)
+}
+
+func BytesToU16(b []byte) uint16 {
+	return binary.BigEndian.Uint16(b)
+}
+func U16ToBytes(v uint16) (val []byte) {
+	binary.BigEndian.PutUint16(val, v)
+	return
+}
+
+func U32ToBytes(v uint32) (val []byte) {
+	binary.BigEndian.PutUint32(val, v)
+	return
 }
