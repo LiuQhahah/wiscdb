15997415fb58d6f8a0bfcf3f47c43cabaf1e8528
qiang_liu <liuqiang9596@gmail.com>
2025-04-21 13:54:23 +0800

    new merge Iterator

---
+++ table/merge_iterator.go
@@ -0,0 +1,42 @@
+package table
+
+import (
+	"wiscdb/internal"
+	"wiscdb/y"
+)
+
+type MergeIterator struct {
+	left    node
+	right   node
+	small   *node
+	curKey  []byte
+	reverse bool
+}
+
+type node struct {
+	valid  bool
+	key    []byte
+	iter   y.Iterator
+	merge  *MergeIterator
+	concat *concatIterator
+}
+
+type concatIterator struct {
+	idx     int
+	cur     *y.Iterator
+	iters   []*internal.Iterator
+	tables  []*Table
+	options int
+}
+
+func NewMergeIterator(iters []y.Iterator, reverse bool) y.Iterator {
+	switch len(iters) {
+	case 0:
+		return nil
+	case 1:
+		return iters[0]
+	case 2:
+		return nil
+	}
+	return nil
+}
