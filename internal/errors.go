14a9498667befaa96ea6461a3b90ee285d9c4d57
qiang_liu <liuqiang9596@gmail.com>
2025-04-25 15:52:52 +0800

    add key-value to pendingWrite in the transaction

--- internal/errors.go
+++ internal/errors.go
@@ -5,5 +5,9 @@ import "errors"
 var (
 	ErrDBClosed = errors.New("DB Closed")
 
-	ErrDiscardTxn = errors.New("This transaction has been discarded. Create a new one")
+	ErrDiscardedTxn = errors.New("This transaction has been discarded. Create a new one")
+	ErrReadOnlyTxn  = errors.New("No sets or deletes are allowed in a read-only transaction")
+	ErrEmptyKey     = errors.New("Key cannot be empty")
+	ErrInvalidKey   = errors.New("Key is using a reserved !wisc! prefix")
+	ErrTxnTooBig    = errors.New("Txn is too big to fit into one request")
 )
