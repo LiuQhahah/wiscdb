908f9dba06899404703c2ffb2a96eefef8ef1a59
qiang_liu <liuqiang9596@gmail.com>
2025-05-14 16:29:39 +0800

    remove sub dir

--- internal/level_manifest.go
+++
@@ -1,5 +0,0 @@
-package internal
-
-type levelManifest struct {
-	Tables map[uint64]struct{}
-}
--- level/targets.go
+++
@@ -1,7 +0,0 @@
-package level
-
-type targets struct {
-	baseLevel int
-	targetSz  []int64
-	fileSz    []int64
-}
